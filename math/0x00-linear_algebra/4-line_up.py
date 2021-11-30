52542bfdfb6ec67e4dca0c3b08327f7ea73e1990
Juanpagab99 <juanpagab99@gmail.com>
2021-11-30 08:15:51 +0700

    task 4 upload

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Add in matrix"""
+
+
+def add_arrays(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        new = []
+        for i in range(len(arr1)):
+            # for j in range(len(arr2))
+            new.append(arr1[i] + arr2[i])
+        return new
