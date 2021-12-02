236e322cbf848f3139658ef274fec3d9fae28151
Juanpagab99 <juanpagab99@gmail.com>
2021-12-02 07:08:27 +0700

    task 2 fixed

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -7,7 +7,7 @@ def matrix_shape(matrix):
     new = []
     try:
         while len(matrix) > 0:
-            print("-", len(matrix))
+            # print("-", len(matrix))
             new.append(len(matrix))
             matrix = matrix[0]
             # print("-", matrix)
