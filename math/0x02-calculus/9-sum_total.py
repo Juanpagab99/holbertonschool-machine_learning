bb1e351beb1b4fdee040cb213ef647c90f50fb34
Juanpagab99 <juanpagab99@gmail.com>
2021-12-10 09:44:31 +0700

    task 9 sum

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Sumatoria"""
+
+
+def summation_i_squared(n):
+    """Sum"""
+    if type(n) is not int or n < 1:
+        return None
+    return int(n * (n + 1) * (2 * n + 1) / 6)
