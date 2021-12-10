ad1c5d592127d254c35e66490bcf94bfb83c4624
Juanpagab99 <juanpagab99@gmail.com>
2021-12-10 09:58:38 +0700

    task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Integrate function"""
+
+
+def poly_integral(poly, C=0):
+    """Integrate all"""
+    if type(poly) is not list and not poly:
+        return None
+    elif len(poly) == 0 and type(C) is not int:
+        return None
+    elif poly == [0]:
+        return [C]
+    else:
+        integral = []
+        integral.append(C)
+        for i in range(len(poly)):
+            x = poly[i] / (i + 1)
+            integral.append(int(x) if x.is_integer() else x)
+        return integral
