e90765c86e8d4e809d710055e5dc64861b692861
Juanpagab99 <juanpagab99@gmail.com>
2021-12-10 09:50:58 +0700

    coment

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,7 +3,7 @@
 
 
 def summation_i_squared(n):
-    """Sum"""
+    """Sumatoria exponencial"""
     if type(n) is not int or n < 1:
         return None
     return int(n * (n + 1) * (2 * n + 1) / 6)
